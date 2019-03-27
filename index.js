c4d9de301fa04b8b26560b6fa56eb783aa1e3c5c
Yakov Liskoff <liskovyakov@gmail.com>
2019-03-27 13:23:07 +0200

    change module export

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'use strict';
-
+/*
 module.exports = require('./lib/rproxyClient');
 module.exports = require('./lib/rproxyRouter');
 module.exports = require('./lib/rproxyServer');
 module.exports = require('./lib/rproxyError');
+*/
+module.exports = {
+    rproxyClient: require('./lib/rproxyClient'),
+    rproxyServer: require('./lib/rproxyServer')
+}
